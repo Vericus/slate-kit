249c34242707a9e1689dfb0fb9e969169723b905
Irwan Fario Subastian <irwan@cadmus.io>
2018-04-07 18:06:32 +1000

    fix rollup trying to build the yeoman generator

--- support/rollup/config.js
+++ support/rollup/config.js
@@ -1,9 +1,14 @@
 import getPackages from "lerna-get-packages";
 import factory from "./factory";
 
-const configurations = getPackages(__dirname).reduce(
-  (acc, { package: pkg, location }) => [...acc, ...factory(pkg, location)],
-  []
-);
+const configurations = getPackages(__dirname)
+  .filter(
+    pkg =>
+      pkg.package["slate-kit"] && pkg.package["slate-kit"].type === "module"
+  )
+  .reduce(
+    (acc, { package: pkg, location }) => [...acc, ...factory(pkg, location)],
+    []
+  );
 
 export default configurations;
