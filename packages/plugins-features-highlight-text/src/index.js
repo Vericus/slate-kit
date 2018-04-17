0d2fad065b93cce3e07929a862388a2eaf42df5e
Irwan Fario Subastian <irwan@cadmus.io>
2018-04-17 16:21:33 +1000

    add utils to get currentActiveColor

--- packages/plugins-features-highlight-text/src/index.js
+++ packages/plugins-features-highlight-text/src/index.js
@@ -2,10 +2,12 @@
 import Options, { type typeOptions } from "./options";
 import createChanges from "./changes";
 import createRenderMark from "./renderMark";
+import createUtils from "./utils";
 
 export default function createPlugin(pluginOptions: typeOptions) {
   const opt = Options.create(pluginOptions);
   const changes = createChanges(opt);
   const renderMark = createRenderMark(opt);
-  return { changes, renderMark };
+  const utils = createUtils(opt);
+  return { changes, utils, renderMark };
 }
---
+++ packages/plugins-features-highlight-text/src/utils.js
@@ -0,0 +1,21 @@
+// @flow
+import type { Value } from "slate";
+import type { typeOptions } from "./options";
+
+export default function createUtils(opts: typeOptions) {
+  const { defaultColor, data, type } = opts;
+  return {
+    currentColor: (value: Value) => {
+      if (value.activeMarks) {
+        const activeColorMarks = value.activeMarks
+          .filter(mark => mark.type === type)
+          .map(mark => mark.data.get(data));
+        const uniqueColorMarks = [...new Set(activeColorMarks)];
+        if (uniqueColorMarks.length !== 0) {
+          return uniqueColorMarks[0];
+        }
+      }
+      return defaultColor;
+    }
+  };
+}
