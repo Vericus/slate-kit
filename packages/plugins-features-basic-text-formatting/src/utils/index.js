bc1599faa7baf894de9a90e46247d58b5d882dc5
Irwan Fario Subastian <irwan@cadmus.io>
2018-05-18 16:05:37 +1000

    reuse hasActiveMark

--- packages/plugins-features-basic-text-formatting/src/utils/index.js
+++ packages/plugins-features-basic-text-formatting/src/utils/index.js
@@ -1,12 +1,12 @@
 // @flow
 import { type Value } from "slate";
-import { hasMark } from "@vericus/slate-kit-plugins-utils";
+import { hasActiveMark } from "@vericus/slate-kit-plugins-utils";
 
 export default function createUtils() {
   return {
-    isBold: (value: Value) => hasMark(value, "bold"),
-    isItalic: (value: Value) => hasMark(value, "italic"),
-    isUnderline: (value: Value) => hasMark(value, "underline"),
-    isStrikethrough: (value: Value) => hasMark(value, "strikethrough")
+    isBold: (value: Value) => hasActiveMark(value, "bold"),
+    isItalic: (value: Value) => hasActiveMark(value, "italic"),
+    isUnderline: (value: Value) => hasActiveMark(value, "underline"),
+    isStrikethrough: (value: Value) => hasActiveMark(value, "strikethrough")
   };
 }
