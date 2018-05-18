dc6a21c513fe0ba38cdc717d07df1405a4347c2e
Irwan Fario Subastian <irwan@cadmus.io>
2018-05-18 16:05:23 +1000

    fixed has activeMark

--- packages/utils/src/hasActiveMark.js
+++ packages/utils/src/hasActiveMark.js
@@ -3,6 +3,9 @@ import type { Value } from "slate";
 
 export default function hasActiveMark(value: Value, type: string): boolean {
   return (
-    value.activeMarks && value.activeMarks.some(mark => mark.type === type)
+    value.selection &&
+    value.selection.isFocused &&
+    value.activeMarks &&
+    value.activeMarks.some(mark => mark.type === type)
   );
 }
