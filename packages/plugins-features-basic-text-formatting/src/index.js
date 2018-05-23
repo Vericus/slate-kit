6679c3ed0898407b57ab195e26b29296b6e7ef47
Irwan Fario Subastian <irwan@cadmus.io>
2018-05-23 18:48:01 +1000

    add mark serializer for basic text format

--- packages/plugins-features-basic-text-formatting/src/index.js
+++ packages/plugins-features-basic-text-formatting/src/index.js
@@ -5,16 +5,20 @@ import createUtils from "./utils";
 import createRenderers from "./renderers";
 import createKeyBindings from "./keyBindings";
 import createSchema from "./schemas";
+import createStyle from "./style";
+import createRule from "./rules";
 
 export default function createBasicTextFormatPlugin(pluginOptions: any = {}) {
   const options = new Options(pluginOptions);
   const changes = createChanges();
   const utils = createUtils();
   const schemas = createSchema(options);
+  const style = createStyle();
+  const rules = createRule;
   const { renderMark } = createRenderers(options.renderMark);
   return {
     plugins: [
-      { options, changes, utils, renderMark, ...schemas },
+      { options, rules, changes, style, utils, renderMark, ...schemas },
       ...createKeyBindings(options, changes)
     ]
   };
---
+++ packages/plugins-features-basic-text-formatting/src/rules/index.js
@@ -0,0 +1,63 @@
+// @flow
+
+const markTags = {
+  strong: "bold",
+  b: "bold",
+  em: "italic",
+  i: "italic",
+  u: "underline",
+  s: "strikethrough",
+  strike: "strikethrough",
+  del: "strikethrough"
+};
+
+export default function createRule(getData) {
+  return [
+    {
+      deserialize(el, next) {
+        const mark = markTags[el.tagName.toLowerCase()];
+        if (!mark) return undefined;
+        if (![...el.childNodes].every(node => node.nodeName === "#text")) {
+          return {
+            object: "mark",
+            type: mark,
+            nodes: next(el.childNodes)
+          };
+        }
+        return {
+          object: "text",
+          leaves: [...el.childNodes].map(node => ({
+            object: "leaf",
+            text: node.nodeValue,
+            marks: [
+              {
+                object: "mark",
+                type: mark
+              }
+            ]
+          }))
+        };
+      }
+    },
+    {
+      deserialize(el) {
+        if (el.nodeName === "#text") {
+          const { parentNode } = el;
+          const { data, marks } = getData(parentNode);
+          return {
+            object: "text",
+            leaves: [
+              {
+                object: "leaf",
+                text: el.nodeValue,
+                data,
+                marks
+              }
+            ]
+          };
+        }
+        return undefined;
+      }
+    }
+  ];
+}
