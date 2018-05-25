50c0fa952b0b950754ee58458223f284b99979a2
Irwan Fario Subastian <irwan@cadmus.io>
2018-05-25 13:16:51 +1000

    expose paste type checker

--- packages/paste-helpers/src/index.js
+++ packages/paste-helpers/src/index.js
@@ -2,7 +2,7 @@ import genericClean from "./generic";
 import wordClean, { isWordHTML } from "./microsoft-word";
 import googleDocsClean, { isGoogleDocsHTML } from "./google-docs";
 
-export default function pasteCleaner(html) {
+function pasteCleaner(html) {
   if (isWordHTML(html)) {
     return { origin: "word", cleanedHTML: wordClean(html) };
   } else if (isGoogleDocsHTML(html)) {
@@ -10,3 +10,5 @@ export default function pasteCleaner(html) {
   }
   return { origin: "generic", cleanedHTML: genericClean(html) };
 }
+
+export { pasteCleaner as default, isWordHTML, isGoogleDocsHTML };
