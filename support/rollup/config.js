7d9971228205c6295ae8adc751d4259643c49a60
Irwan Fario Subastian <irwan@cadmus.io>
2018-08-20 13:21:37 +1000

    fix build order

--- support/rollup/config.js
+++ support/rollup/config.js
@@ -7,6 +7,24 @@ const configurations = getPackages(path.resolve("."))
     pkg =>
       pkg.package["slate-kit"] && pkg.package["slate-kit"].type === "module"
   )
+  .map(pkg => {
+    if (/renderer/gi.test(pkg.package.name)) {
+      return {
+        ...pkg,
+        buildOrder: 1
+      };
+    } else if (/util/gi.test(pkg.package.name)) {
+      return {
+        ...pkg,
+        buildOrder: 0
+      };
+    }
+    return {
+      ...pkg,
+      buildOrder: 2
+    };
+  })
+  .sort((pkg1, pkg2) => pkg1.buildOrder - pkg2.buildOrder)
   .reduce(
     (acc, { package: pkg, location }) => [...acc, ...factory(pkg, location)],
     []
