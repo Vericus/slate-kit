5d5b5234f9a875f1d43ea50a504a9dbba676b7d0
Irwan Fario Subastian <irwan@cadmus.io>
2018-10-26 11:04:44 +1100

    add a way to add changeArgs to keybind

--- packages/plugins-helpers-bind-hotkey/src/onKeyDown.ts
+++ packages/plugins-helpers-bind-hotkey/src/onKeyDown.ts
@@ -4,15 +4,20 @@ import Options from "./options";
 
 function createOnKeyDown(opts: Options) {
   const { hotkeys } = opts;
-  const { change: changeFn } = opts;
+  const { change: changeFn, changeArgs } = opts;
   const hotkeyArrays = Array.isArray(hotkeys) ? hotkeys : [hotkeys];
+  console.log(changeArgs);
 
   function checkHotKey(event): boolean {
     return hotkeyArrays.some(hotkey => isHotkey(hotkey)(event));
   }
   return (event, change: Change): void | Change | boolean => {
     if (checkHotKey(event)) {
-      change.call(changeFn);
+      if (changeArgs) {
+        change.call(changeFn, changeArgs);
+      } else {
+        change.call(changeFn);
+      }
       return true;
     }
     return undefined;
--- packages/plugins-helpers-bind-hotkey/src/options.ts
+++ packages/plugins-helpers-bind-hotkey/src/options.ts
@@ -4,13 +4,19 @@ import { Change } from "slate";
 export interface TypeOptions {
   hotkeys?: string | string[];
   change?: (change: Change) => Change;
+  changeArgs?: any[];
 }
 
-const defaultOptions: TypeOptions = { hotkeys: undefined, change: undefined };
+const defaultOptions: TypeOptions = {
+  hotkeys: undefined,
+  change: undefined,
+  changeArgs: undefined
+};
 
 class Options extends Record(defaultOptions) {
   hotkeys: string | string[];
   change: (change: Change) => Change;
+  changeArgs?: any[];
 }
 
 export default Options;
