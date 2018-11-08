97d7d73b3d46d0a6f11d6afa8a9cc9f29c034032
Irwan Fario Subastian <irwan@cadmus.io>
2018-11-08 11:57:13 +1100

    simplified undo/redo

--- packages/plugins-features-history/src/commands/handleRedo.ts
+++ packages/plugins-features-history/src/commands/handleRedo.ts
@@ -5,16 +5,9 @@ export default function handleRedo(
   editor: Editor,
   onRedo?: (operations: List<Operation>) => void
 ) {
-  const { value } = editor;
-  if (!editor.hasRedo(value)) return;
-  const redos = value.data.get("redos");
-  redos.some(redo => {
-    editor.redo();
-    if (redo.size === 1 && redo.get(0).type === "set_selection") {
-      return false;
-    }
-    return true;
-  });
+  if (!editor.hasRedo()) return;
+  editor.redo();
+
   if (onRedo && typeof onRedo === "function") {
     onRedo(
       editor.operations.filter(operation => operation.type !== "set_value")
--- packages/plugins-features-history/src/commands/handleUndo.ts
+++ packages/plugins-features-history/src/commands/handleUndo.ts
@@ -5,20 +5,8 @@ export default function handleUndo(
   editor: Editor,
   onUndo?: (operations: List<Operation>) => void
 ) {
-  const { value } = editor;
-  if (!editor.hasUndo(value)) return;
-  const undos = value.data.get("undos");
-  undos.some(undo => {
-    console.log(editor.operations);
-    if (
-      undo.size === 1 &&
-      (undo.get(0).type === "set_selection" || undo.get(0).type === "set_value")
-    ) {
-      return false;
-    }
-    editor.undo();
-    return true;
-  });
+  if (!editor.hasUndo()) return;
+  editor.undo();
   if (onUndo && typeof onUndo === "function") {
     onUndo(
       editor.operations.filter(operation => operation.type !== "set_value")
