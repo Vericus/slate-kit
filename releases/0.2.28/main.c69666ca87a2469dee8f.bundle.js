69f84cafce26174caf4e399d4c990fe8482390f3
Irwan Fario Subastian <irwan@cadmus.io>
2019-04-01 11:28:59 +1100

    update github pages with version  0.2.28

---
+++ releases/0.2.28/main.c69666ca87a2469dee8f.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(404),p(481),n.exports=p(1062)},481:function(n,o,p){"use strict";p(482),p(704),p(709),p(864),p(867),p(1059)}},[[0,1,2]]]);
